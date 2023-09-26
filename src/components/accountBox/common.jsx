291edef38eee8fbf206858313e6ad4d7904527d9
tingzcb <tingting.sun@cqumail.com>
2023-09-26 17:21:51 +1000

    aadd yup validation

--- src/components/accountBox/common.jsx
+++ src/components/accountBox/common.jsx
@@ -75,3 +75,15 @@ export const SubmitButton = styled.button`
     filter: brightness(1.03);
   }
 `;
+
+export const FieldContainer = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+`;
+
+export const FieldError = styled.span`
+  color: #b32e2e;
+  font-size: 11px;
+  min-height: 18px;
+`;
--- src/components/accountBox/signupForm.jsx
+++ src/components/accountBox/signupForm.jsx
@@ -2,6 +2,8 @@ import React, { useContext } from "react";
 import {
   BoldLink,
   BoxContainer,
+  FieldContainer,
+  FieldError,
   FormContainer,
   Input,
   MutedLink,
@@ -13,40 +15,74 @@ import { useFormik } from "formik";
 //
 import * as yup from 'yup';
 
-const vali
 
+
+const PASSWORD_REGEX = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/;
+
+//password regex for strong password
+//confirmPassword when then
+const validationSchema = yup.object({
+  fullName: yup.string().min(3, "Please enter your real name").required('Full name is required!'),
+  email: yup.string().email("Please enter a valid email address").required(),
+  password: yup
+    .string()
+    .matches(PASSWORD_REGEX, "Please enter a strong password")
+    .required(),
+  confirmPassword: yup
+    .string()
+    .required("Please confirm your password")
+    .when("password", {
+      is: (val) => (val && val.length > 0 ? true : false),
+      then: ()=> yup
+        .string()
+        .oneOf([yup.ref("password")], "Password does not match"),
+    }),
+});
 export function SignupForm(props) {
   const { switchToSignin } = useContext(AccountContext);
 
   //onSubmit call back will be called every time the form click submit,
   // is the method FormitConfig<>.onSubmit
-  const onSubmit = (values)=>{
+  const onSubmit = (values) => {
     alert(JSON.stringify(values));
   };
 
-// the const formik is passed in the FormContainer component onSubmit handler 
+  // the const formik is passed in the FormContainer component onSubmit handler 
   const formik = useFormik({
-    initialValues:{fullName: '', email:'', password:'', confirmPassword:''},
+    initialValues: { fullName: '', email: '', password: '', confirmPassword: '' },
     validateOnBlur: true,
     onSubmit,
+    validationSchema: validationSchema,
 
   });
 
-
+  console.log("Error:", formik.errors);
 
 
   return (
     <BoxContainer>
       <FormContainer onSubmit={formik.handleSubmit}>
-        <Input name="fullName"  type="text" placeholder="Full Name"  value={formik.values.fullName} onChange={formik.handleChange}/>
-        <Input name="email" type="email" placeholder="Email" value={formik.values.email} onChange={formik.handleChange}/>
-        <Input  name="password" type="password" placeholder="Password" value={formik.values.password} onChange={formik.handleChange}/>
-        <Input  name= "confirmPassword"  type="password" placeholder="Confirm Password" 
-         value={formik.values.confirmPassword}  onChange={formik.handleChange}/>
-         <Marginer direction="vertical" margin="1em" />
-         <SubmitButton type="submit">Signup</SubmitButton>
+        <FieldContainer>
+          <Input name="fullName" type="text" placeholder="Full Name" value={formik.values.fullName} onChange={formik.handleChange} />
+          <FieldError>{formik.touched.fullName && formik.errors.fullName ? formik.errors.fullName : ""}</FieldError>
+        </FieldContainer>
+        <FieldContainer>
+          <Input name="email" type="email" placeholder="Email" value={formik.values.email} onChange={formik.handleChange} />
+          <FieldError>{formik.touched.email && formik.errors.email ? formik.errors.email : ""}</FieldError>
+        </FieldContainer>
+        <FieldContainer>
+          <Input name="password" type="password" placeholder="Password" value={formik.values.password} onChange={formik.handleChange} />
+          <FieldError>{formik.touched.password && formik.errors.password ? formik.errors.password : ""}</FieldError>
+        </FieldContainer>
+        <FieldContainer>
+          <Input name="confirmPassword" type="password" placeholder="Confirm Password"
+            value={formik.values.confirmPassword} onChange={formik.handleChange} />
+          <FieldError>{formik.touched.confirmPassword && formik.errors.confirmPassword ? formik.errors.confirmPassword : ""}</FieldError>
+        </FieldContainer>
+        <Marginer direction="vertical" margin="1em" />
+        <SubmitButton type="submit">Signup</SubmitButton>
       </FormContainer>
-      <Marginer direction="vertical" margin={10} />     
+      <Marginer direction="vertical" margin={10} />
       <MutedLink href="#">
         Already have an account?
         <BoldLink href="#" onClick={switchToSignin}>
